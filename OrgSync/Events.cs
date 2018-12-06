423076c9d94ec8d4d56c4adc929f6a46693295a5
Salma Shayeb <Salma.Shayeb-1@ou.edu>
2018-12-06 16:08:02 -0600

    class events

---
+++ OrgSync/Events.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrgSync
+{
+    class Events
+    {
+        public string EventType { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime DayTime { get; set; }
+
+    }
+}
